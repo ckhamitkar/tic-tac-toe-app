779a397acfb0b314b7c66f8223b4a2c83c89e0c6
Anjali Khamitkar <akhamitkar@yahoo.com>
2023-12-17 18:23:22 -0500

    Reviewed working code for reseting app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const initialState = Array(9).fill(null);
+
+const App = () => {
+  const [board, setBoard] = useState(initialState);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (index) => {
+    const newBoard = [...board];
+
+    if (calculateWinner(newBoard) || newBoard[index]) {
+      return;
+    }
+
+    newBoard[index] = xIsNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const resetGame = () => {
+    setBoard(initialState);
+    setXIsNext(true);
+  };
+
+  const renderSquare = (index) => (
+    <button className="square" onClick={() => handleClick(index)}>
+      {board[index]}
+    </button>
+  );
+
+  const winner = calculateWinner(board);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else if (board.every((square) => square)) {
+    status = 'It\'s a draw!';
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <h1>Charu's Tic-Tac-Toe React App</h1>
+      <div className="game-board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <button className="reset-button" onClick={resetGame}>
+          Reset
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
 
 export default App;
+
